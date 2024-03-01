5d82034916d72e8fdedaba0ea60cf60f943b0585
VilkhovyiDmytro <dmitrovilhovij@gmail.com>
2024-03-01 10:20:35 +0200

    modify modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,14 @@
 import { createPortal } from "react-dom";
+import { useOutsideClick } from "../helpers/useOutsideClick";
 
 const Modal = ({ children, onCloseModal, title = "" }) => {
+  const ref = useOutsideClick(onCloseModal);
   return createPortal(
     <div className="fixed top-0 right-0 bg-slate-500/35 h-full w-full flex justify-center items-center">
-      <section className="w-3/5 h-3/5 bg-slate-100 rounded-2xl max-w-[550px] px-6 py-4 text-xl">
+      <section
+        className="w-3/5 h-3/5 bg-slate-100 rounded-2xl max-w-[550px] px-6 py-4 text-xl"
+        ref={ref}
+      >
         <header className=" flex items-center justify-between">
           <h2 className="font-semibold tracking-wider">{title}</h2>
           <span
