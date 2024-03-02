c0860347cb93c367baf5603b38d4124f73c865e2
ClydeUrov <clydeurov@gmail.com>
2024-03-02 18:59:55 +0200

    Fix some dep bugs

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -5,6 +5,8 @@ import { AiOutlinePicture } from "react-icons/ai";
 import NavLinks from "../components/NavLinks";
 import { linksForStudent } from "../data/linksForStudent";
 import { linksForTeachers } from "../data/linksForTeacers";
+import { FaRegBell } from "react-icons/fa6";
+import { IoSettings } from "react-icons/io5";
 
 
 const UserPage = () => {
@@ -15,6 +17,13 @@ const UserPage = () => {
     state: "Teacher",
     // state: "Student",
   });
+
+  const userRole =
+    userData.state === "Student"
+      ? "Студента"
+      : userData.state === "Admin"
+      ? "Адміністратора"
+      : "Викладача";
 
   const handleLogout = () => {};
 
